2873cd8d3bc5a94a7fc60301d5930f83f83e343b
mecodesta <the3drevolution@gmail.com>
2016-04-06 10:17:57 -0700

    Ionic Modals and Forms

--- Week 2/ionic/conFusion/www/js/controllers.js
+++ Week 2/ionic/conFusion/www/js/controllers.js
@@ -11,13 +11,35 @@ angular.module('conFusion.controllers', [])
 
   // Form data for the login modal
   $scope.loginData = {};
-
+  $scope.reservation = {};
   // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
     scope: $scope
   }).then(function(modal) {
     $scope.modal = modal;
   });
+
+  //Create the reserve table modal
+  $ionicModal.fromTemplateUrl('templates/reserve.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.reserveform = modal;
+  });
+
+  $scope.closeReserve = function() {
+    $scope.reserveform.hide();
+  };
+
+  $scope.reserve = function() {
+    $scope.reserveform.show();
+  };
+
+  $scope.doReserve = function() {
+    console.log('Doing Reservation for user: ', $scope.loginData.username);
+    $timeout(function(){
+      $scope.closeReserve();
+    }, 1000);
+  };
 
   // Triggered in the login modal to close it
   $scope.closeLogin = function() {
@@ -31,7 +53,7 @@ angular.module('conFusion.controllers', [])
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+    console.log('Doing login; user: ', $scope.loginData.username, '; password: ', $scope.loginData.password);
 
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
