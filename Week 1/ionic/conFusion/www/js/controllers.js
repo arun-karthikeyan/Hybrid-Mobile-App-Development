32521fd1266d570dd593eb3fe57d9bf63e77595d
mecodesta <the3drevolution@gmail.com>
2016-04-06 07:15:47 -0700

    Week1 Assignment - updated aboutus and contactus page

--- Week 1/ionic/conFusion/www/js/controllers.js
+++ Week 1/ionic/conFusion/www/js/controllers.js
@@ -178,9 +178,10 @@ menuFactory.getDishes().update({id:$scope.dish.id},$scope.dish);
 
             }])
 
-.controller('AboutController', ['$scope', 'corporateFactory', function($scope, corporateFactory) {
+.controller('AboutController', ['$scope', 'corporateFactory', 'baseURL', function($scope, corporateFactory, baseURL) {
 
             $scope.leaders = corporateFactory.query();
-            console.log($scope.leaders);
+            $scope.baseURL = baseURL;
+            //console.log($scope.leaders);
 
             }]);
